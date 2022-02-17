b578c37bf0f4d29018bae950627314751f00b946
Daniil <pechenkin031199@mail.ru>
2022-02-17 16:57:38 +0500

    Add project files.

---
+++ Automated.Course.System.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Automated.Course.System.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
